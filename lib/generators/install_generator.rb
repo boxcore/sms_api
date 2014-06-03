50f1b9c42d5de6b5e2c60c958db6f23892f9d5ca
bruce <zth5906@gmail.com>
2014-06-03 16:08:59 +0800

    version

--- lib/generators/install_generator.rb
+++ lib/generators/install_generator.rb
@@ -1,10 +1,12 @@
 # encoding: utf-8
 module SmsApi
-  class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path('../templates', __FILE__)
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path('../templates', __FILE__)
 
-    def create_initializer_file
-      template 'initializer.rb', 'config/initializers/sms_api.rb'
+      def create_initializer_file
+        template 'initializer.rb', 'config/initializers/sms_api.rb'
+      end
     end
   end
 end
--- lib/sms_api/version.rb
+++ lib/sms_api/version.rb
@@ -1,4 +1,4 @@
 # encoding: utf-8
 module SmsApi
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
